59439c8c304f809b980432335435cb94feae8563
Finn Fassbender <nnffssbndr@gmail.com>
2022-10-16 20:37:19 +0200

    removed unecessary fluff and added datetime for list-of-lists generation | currently not working -> unable to open .pth file

--- Classifiers/evaluation.py
+++ Classifiers/evaluation.py
@@ -1,21 +1,14 @@
 import numpy as np
 import tensorflow as tf
-from datasets import load_dataset
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import losses
-from tensorflow.keras import utils
-from tensorflow.keras.layers import TextVectorization
 import json
-from sklearn.metrics import classification_report
-import tensorflow_addons as tfa
 import re
 import csv
+from dateutil.parser import parse as parse_dt
+from datetime import datetime
 
 # Loading the Model
-####################################################################
-model = tf.keras.models.load_model('model_keras_big')
+#--------------------------------------------------------------------
+model = tf.keras.models.load_model('Classifiers/model700e_1e-4wd.pth')
 
 model.compile(
     loss='sparse_categorical_crossentropy',
@@ -23,14 +16,14 @@ model.compile(
     metrics=['accuracy']
 )
 
-
 model.summary()
-####################################################################
 
+
+####################################################################
 
 
 # Help functions to make and evalate predictions
-####################################################################
+#--------------------------------------------------------------------
 def text_to_list_of_sentences(text_list):
     text_list = text_list.split('.')
     return text_list
@@ -61,6 +54,7 @@ def sum_over_parts(list_of_parts):
             
     return sums_of_emotions
 
+
 def wholePrediction(tweet_list_by_day):
     daily_emotions = []
     for day_of_tweets in tweet_list_by_day:
@@ -77,26 +71,19 @@ def predictEmotions(list_of_texts):
         emotional_list = [emotions[0][0], emotions[0][1], emotions[0][2], emotions[0][3], emotions[0][4], emotions[0][5]]
         detected_emotions.append(emotional_list)
     return detected_emotions
+
+
 #####################################################################
 
 
+# Helper functions to read and filter the right tweet text
+#--------------------------------------------------------------------
 
-#Help functions to read and filter the right tweet text
-#####################################################################
-
-#removed Links aus Eingabestring
-def removeUrl(tweet):
-    tweet["full_text"] = re.sub(r" http\S+", "", tweet["full_text"])
-    return tweet
-
-#removed hashtags aus eingabestring
-def removeHashtag(tweet):
-    tweet["full_text"] = re.sub(r" #\S+", "", tweet["full_text"])
-    return tweet
-
-#
-def removeAT(tweet):
-    tweet["full_text"] = re.sub(r" @\S+", "", tweet["full_text"])
+# entfernt Links, Hashtags, Mentions aus Eingabestring
+def removeFluff(tweet):
+    tweet["full_text"] = re.sub(r" http\S+", "", tweet["full_text"]) #remove URL
+    tweet["full_text"] = re.sub(r" #\S+", "", tweet["full_text"])    #remove #
+    tweet["full_text"] = re.sub(r" @\S+", "", tweet["full_text"])    #remove @
     return tweet
 
 #Schaut ob Eingabestring ein reply ist
@@ -105,37 +92,34 @@ def isReply(tweet):
 
 # Schaut ob Eingabestring kürzer ist als drei Worte
 def islongerthanthreewords(tweet):
-    list_of_words = tweet["full_text"].split()
-    return len(list_of_words) > 3
+    return len(tweet["full_text"].split()) > 3
 
-
-# Wandelt Json in Liste an Listen mit jeweil 5000 Texten (5000 random Tweets von einem Tag)
+# Wandelt json in Liste von Listen um, mit jeweils 5000 Texten (5000 random Tweets von einem Tag)
 def json_to_listOfTexts(json_list):        
     tweet_list_by_day = []
     one_day_of_tweets = []
+    date = datetime.date(2022, 2, 22)
     for i in range(len(json_list)):
-        if i % 4050 == 0 and i > 0:
+        onetweet = removeFluff(json.loads(json_list[i]))
+        # if tweet is not from current date, but newer -> create new sublist
+        if parse_dt(onetweet["created_at"]).date() > date:
             tweet_list_by_day.append(one_day_of_tweets)
             one_day_of_tweets = []
-        onetweet = json.loads(json_list[i])
-        onetweet = removeAT(onetweet)
-        onetweet = removeHashtag(onetweet)
-        onetweet = removeUrl(onetweet)
+            date = parse_dt(onetweet["created_at"]).date()
+            print(date) # print date for progress and sanity check
         if onetweet["lang"] == "en" and islongerthanthreewords(onetweet) and not isReply(onetweet): 
             one_day_of_tweets.append(onetweet["full_text"])
     return tweet_list_by_day
-
 
 
 ######################################################################
 
 
 # Tweets einlesen
-with open('x00Ukraine_Krieg_Tweets.jsonl', 'r') as json_file:
+with open('../../Ukraine_Krieg_Tweets.jsonl', 'r') as json_file:
     json_list = list(json_file)
 
-
-#Tweets einleses, filtern und predictions machen
+#Tweets einlesen, filtern und predictions machen
 tweet_list_by_day = json_to_listOfTexts(json_list)
 predictions = wholePrediction(tweet_list_by_day)
 print(predictions)
